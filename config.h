4c1d1c5aa2f97a4e024ead076fcb135235f41852
Tobias Happ <tobias.happ@gmx.de>
2017-05-22 13:17:05 +0200

    Adds shortcuts for chrome and qpdf

--- config.h
+++ config.h
@@ -63,8 +63,10 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_main, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "uxterm", NULL };
+static const char *dmenucmd[]  = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_main, "-sf", col_gray4, NULL };
+static const char *termcmd[]   = { "uxterm", NULL };
+static const char *qpdfcmd[]   = { "qpdfview", NULL };
+static const char *chromecmd[] = { "google-chrome-stable", NULL };
 
 static const char *upvol[]   = { "amixer", "set", "Master", "1%+", NULL };
 static const char *downvol[] = { "amixer", "set", "Master", "1%-", NULL };
@@ -79,6 +81,8 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ALT,                   XK_p,      spawn,          {.v = qpdfcmd } },
+	{ MODKEY|ALT,                   XK_g,      spawn,          {.v = chromecmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
