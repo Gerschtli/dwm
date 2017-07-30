ca9f46d31edc5216f82a6171eb830384fe0c1dc5
Tobias Happ <tobias.happ@gmx.de>
2017-07-31 00:18:52 +0200

    Use chrome exec instead of google-chrome-stable

--- config.def.h
+++ config.def.h
@@ -67,7 +67,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[]  = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_main, "-sf", col_gray4, NULL };
 static const char *termcmd[]   = { "uxterm", NULL };
 static const char *qpdfcmd[]   = { "qpdfview", NULL };
-static const char *chromecmd[] = { "google-chrome-stable", NULL };
+static const char *chromecmd[] = { "chrome", NULL };
 
 static const char *upvol[]   = { "amixer", "set", "Master", "1%+", NULL };
 static const char *downvol[] = { "amixer", "set", "Master", "1%-", NULL };
