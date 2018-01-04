f6ad32523dec846533e18b4ffd737761633c2559
Tobias Happ <tobias.happ@gmx.de>
2018-01-04 23:46:02 +0100

    Set background color

--- config.def.h
+++ config.def.h
@@ -18,6 +18,7 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_main[]        = "#540303";
+static const char col_bg[]          = "#1A0303";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
