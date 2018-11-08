14d487dbcf4251a9031c81fd65c4ac47c6058f0e
Tobias Happ <tobias.happ@gmx.de>
2018-11-08 16:12:58 +0100

    Change termcmd to urxvt

--- config.def.h
+++ config.def.h
@@ -91,7 +91,7 @@ spawnscreenshot(const Arg* arg)
 
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[]  = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_main, "-sf", col_gray4, NULL };
-static const char *termcmd[]   = { "uxterm", NULL };
+static const char *termcmd[]   = { "urxvt", NULL };
 static const char *qpdfcmd[]   = { "qpdfview", NULL };
 static const char *chromecmd[] = { "chrome", NULL };
 
