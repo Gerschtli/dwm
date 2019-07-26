0c47589d5a851d7947d768eda8fc27b492840c5d
Tobias Happ <tobias.happ@gmx.de>
2019-07-27 01:27:48 +0200

    Add new travis and patch setup

---
+++ create-patch.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+VERSION="${1}"
+FILE="${2:-patch-"${1}".diff}"
+
+git config --local diff.mnemonicprefix true
+git diff --ignore-space-at-eol "${VERSION}" --  . ':!*.diff' ':!.git*' ':!.travis.yml' ':!create-patch.sh' > "${FILE}"
