2ddc887be7d46408638d7d1e2db41737f181558a
Tobias Happ <tobias.happ@gmx.de>
2021-11-07 20:38:02 +0100

    Add flake setup

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Customized dwm";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+
+      overlay = final: prev: {
+        dwm = prev.dwm.overrideAttrs (old: {
+          src = builtins.path { path = ./.; name = "dwm"; };
+        });
+      };
+    in
+    {
+      inherit overlay;
+
+      checks.${system}.build = (
+        import nixpkgs {
+          inherit system;
+          overlays = [ overlay ];
+        }
+      ).dwm;
+    };
+}
