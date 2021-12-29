2d9f6a35f5efa28cec84281fa68f43660e578da7
Tobias Happ <tobias.happ@gmx.de>
2021-12-29 22:02:17 +0100

    Replace urxvt with alacritty

--- config.def.h
+++ config.def.h
@@ -63,7 +63,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[]  = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_main, "-sf", col_gray4, NULL };
-static const char *termcmd[]   = { "urxvt", NULL };
+static const char *termcmd[]   = { "alacritty", NULL };
 static const char *qpdfcmd[]   = { "qpdfview", NULL };
 static const char *chromecmd[] = { "google-chrome-stable", NULL };
 
