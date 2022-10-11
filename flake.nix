bbc514735a91bc3fb4fc19f9deb04247660fb999
Tobias Happ <tobias.happ@gmx.de>
2022-10-11 16:18:06 +0200

    Add version check in flake

--- flake.nix
+++ flake.nix
@@ -9,18 +9,35 @@
 
       overlay = final: prev: {
         dwm = prev.dwm.overrideAttrs (old: {
+          version = "6.4";
           src = builtins.path { path = ./.; name = "dwm"; };
         });
       };
-    in
-    {
-      overlays.default = overlay;
 
-      checks.${system}.build = (
+      dwm = (
         import nixpkgs {
           inherit system;
           overlays = [ overlay ];
         }
       ).dwm;
+    in
+    {
+      overlays.default = overlay;
+
+      packages.${system}.default = dwm;
+
+      checks.${system} = {
+        build = dwm;
+
+        version = nixpkgs.legacyPackages.${system}.runCommand "version-check" { } ''
+          dwm_version="$(${dwm}/bin/dwm -v 2>&1 || :)"
+
+          echo "package version: ${dwm.name}"
+          echo "dwm version:     $dwm_version"
+
+          [[ "${dwm.name}" == "$dwm_version" ]]
+          touch ${placeholder "out"}
+        '';
+      };
     };
 }
